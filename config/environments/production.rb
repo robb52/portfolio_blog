1af6526e675ea1240d0f7c3eaaaac4ddaa0a9729
Rob Brown <brown4homes@yahoo.com>
2019-01-31 15:36:28 -0800

    Added domain to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,11 +97,11 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'https://portfolio-blog-app.herokuapp.com/',
     :enable_starttls_auto => true
   }
 end
