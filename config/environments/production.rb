a4e11832c92d9a48ec7507d8d4daf667df6150a8
Rob Brown <brown4homes@yahoo.com>
2019-01-31 14:05:25 -0800

    Reconfig sendgrid in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'portfolio-blog-app.herokuapp.com',
+    :form                 => 'brown4homes@yahoo.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => 'heroku.com',
+    :address              => 'smtp.sendgrid.net',
+    :port                 => 587,
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 end
